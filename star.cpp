b8cd4ca469701ffaa0eab1108c89006e136a8253
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-09-22 22:37:27 +0530

    first commit

---
+++ star.cpp
@@ -0,0 +1,36 @@
+#include <GL/glut.h>
+#include <math.h>
+void line_join(float x1 , float y1 , float x2 , float y2){
+    
+    glBegin(GL_LINES);
+    glVertex2f(x1,y1);
+    glVertex2f(x2,y2);
+    
+}
+
+void cirlce()
+{
+    float theta;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+    for(int i=0; i<360; i++)
+    {
+        theta = i*3.142/180;
+        glVertex2f(0.125*cos(theta),0.125*sin(theta));
+    }
+    glEnd();
+    glFlush();
+}
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_LINES);
+    
+    glColor3f(0.0,0.0,1.0);
+    
+    line_join(0.0, 1.0, -0.86, -0.50);
+    line_join(0.0, 1.0, 0.86, -0.50);
+    line_join(-0.86, -0.50, 0.86, -0.50);
+    
+    glColor3f(1.0,0.0,1.0);
+    
