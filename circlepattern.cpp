c930264685613d63aa032835e74c3aec3e3da043
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-09-24 22:51:45 +0530

    circle pattern

--- circlepattern.cpp
+++ circlepattern.cpp
@@ -288,3 +288,74 @@ glBegin(GL_LINES);
              glVertex2f(40*cos(theta2),40*sin(theta2));
         }
         glEnd();
+
+        glBegin(GL_POLYGON);
+        glColor3f(0.0,0.0,0.0);
+        for(int i=0;i<360;i++)
+        {
+            float theta1= (i+0.5)*3.142/180;
+            glVertex2f(30*cos(theta1),30*sin(theta1));
+        }
+        glEnd();
+
+
+        glBegin(GL_LINES);
+        glColor3f(0.0,0.1,0.9);
+        rad = 30;
+        x=0.0f;y=0.0f;
+        while(x<=rad)
+        {
+            y= sqrt((rad*rad - x*x));
+            glVertex2f(x,y);
+            glVertex2f(x,-y);
+            glVertex2f(-x,y);
+            glVertex2f(-x,-y);
+            glVertex2f(y,x);
+            glVertex2f(-y,x);
+            glVertex2f(y,-x);
+            glVertex2f(-y,-x);
+            x=x+2;
+        }
+        glEnd();
+
+        glBegin(GL_LINES);
+        glColor3f(0.1,0.3,1.0);
+        for(int i=0;i<360;i++)
+            {
+             float theta1= (i+0.5)*3.142/180;
+             float theta2= (i-0.5)*3.142/180;
+             glVertex2f(30*cos(theta1),30*sin(theta1));
+             glVertex2f(30*cos(theta2),30*sin(theta2));
+        }
+        glEnd();
+
+
+
+        glBegin(GL_POLYGON);
+        glColor3f(0.0,0.0,0.0);
+        for(int i=0;i<360;i++)
+        {
+            float theta1= (i+0.5)*3.142/180;
+            glVertex2f(20*cos(theta1),20*sin(theta1));
+        }
+        glEnd();
+
+
+        glBegin(GL_LINES);
+        glColor3f(0.5,0.5,0.0);
+        rad = 20;
+        x=0.0f;y=0.0f;
+        while(x<=rad)
+        {
+            y= sqrt((rad*rad - x*x));
+            glVertex2f(x,y);
+            glVertex2f(x,-y);
+            glVertex2f(-x,y);
+            glVertex2f(-x,-y);
+            glVertex2f(y,x);
+            glVertex2f(-y,x);
+            glVertex2f(y,-x);
+            glVertex2f(-y,-x);
+            x=x+2;
+        }
+        glEnd();
