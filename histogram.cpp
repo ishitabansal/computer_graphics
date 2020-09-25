b16881a781e367530fa035e980ae8cdd393e6c31
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-09-25 21:41:14 +0530

    second commit

--- histogram.cpp
+++ histogram.cpp
@@ -31,3 +31,33 @@ void display()
 
      glBegin(GL_LINES);
      glColor3f(0.0,1.0,0.9);
+    for(int i=-70;i<-49; i=i+2)
+     {
+         glVertex2f(i,70);
+         glVertex2f(i,-90);
+     }
+
+     for(int i=70;i>-90;i=i-2)
+     {
+         glVertex2f(-70,i);
+         glVertex2f(-50,i);
+     }
+     glEnd();
+
+
+     glBegin(GL_LINES);
+     glColor3f(0.0,0.0,1.0);
+
+     for(int i=-40;i<-19; i=i+2)
+     {
+         glVertex2f(i,10);
+         glVertex2f(i,-90);
+     }
+
+     for(int i=10;i>-90;i=i-2)
+     {
+         glVertex2f(-40,i);
+         glVertex2f(-20,i);
+     }
+     glEnd();
+
