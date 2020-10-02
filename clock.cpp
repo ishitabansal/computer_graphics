67a85fde87133bb302ec72f318c85a53645f6b23
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-10-02 22:27:38 +0530

    first commit

---
+++ clock.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+#include <math.h>
+
+struct Point {
+	GLint x;
+	GLint y;
+};
+
+void draw_dda(Point p1, Point p2) {
+	GLfloat dx = p2.x - p1.x;
+	GLfloat dy = p2.y - p1.y;
+
+	GLfloat x1 = p1.x;
+	GLfloat y1 = p1.y;
+
+	GLfloat step = 0;
+
+	if(abs(dx) > abs(dy)) {
+		step = abs(dx);
+	} else {
+		step = abs(dy);
+	}
+
+	GLfloat xInc = dx/step;
+	GLfloat yInc = dy/step;
+
+	for(float i = 1; i <= step; i++) {
+		glVertex2i(x1, y1);
+		x1 += xInc;
+		y1 += yInc;
+	}
+}
