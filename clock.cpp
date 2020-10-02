6435bb873ea89428490e5626c6870981b85879a7
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-10-02 22:28:13 +0530

    second commit

--- clock.cpp
+++ clock.cpp
@@ -30,3 +30,35 @@ void draw_dda(Point p1, Point p2) {
 		y1 += yInc;
 	}
 }
+void init() {
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glPointSize(1.0f);
+	gluOrtho2D(0.0f, 640.0f, 0.0f, 480.0f);
+}
+
+void draw_circle(Point pC, GLfloat radius) {
+	GLfloat step = 1/radius;
+	GLfloat x, y;
+
+	for(GLfloat theta = 0; theta <= 360; theta += step) {
+		x = pC.x + (radius * cos(theta));
+		y = pC.y + (radius * sin(theta));
+		glVertex2i(x, y);
+	}
+}
+
+Point pC = {200, 200};
+GLint radius = 150;
+
+// radius of the needles from  the center
+GLint hRadius = 120;
+GLint mRadius = 130;
+GLint sRadius = 140;
+
+// angles of the three needles
+double hDegree = 0;
+double mDegree = 0;
+double sDegree = 0;
+
+void display(void) {
